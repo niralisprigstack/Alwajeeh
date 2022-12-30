610b0193887e6a5ffc6a75802b471887e71cc32c
ADMIN <nirali.sprigstack@outlook.com>
2022-12-30 13:28:46 +0530

    dashboard related changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,15 +1,16 @@
 
 @if(isset($headtext) || isset($subheadtext))
 <header class="fixed-header">
-<!-- <a class="dropdown-item navDropdown pt-2 pb-2 nav-border t-center dropdownpadd fonthead" href="{{ route('logout') }}" onclick="event.preventDefault();
+
+<div class="staticBlackBg top-zero">
+<a class="d-none" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                             document.getElementById('logout-form').submit();">
                                                     {{ __('Logout') }}
                                                 </a>
 
                                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                                     @csrf
-                                                </form> -->
-<div class="staticBlackBg top-zero">
+                                                </form>
         <img class="img-fluid" style="width: 100px;height: 104.39px;float: right;" src="{{ asset('assests/images/register_logo.svg') }}" alt="">
         <div class="commonheadertext">
         <span class="announcementText">{{$headtext}}</span>
